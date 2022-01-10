56116e272f68e3727751d5b4d37c1cc458773215
leeqingxiao100 <1418349266@qq.com>
2022-01-10 17:21:39 +0800

    lodash

--- lodash/leeqingxiao100-lodash.js
+++ lodash/leeqingxiao100-lodash.js
@@ -1,6 +1,17 @@
 var leeqingxiao100 = {
   chunk: function (array, size) {
-
+    let res = []
+    for (var i = 0; i < array.length; i++) {
+      var num = []
+      for (var j = 0; j < size; j++) {
+        num[j] = array[i]
+        if (i == array.length - 1) {
+          break
+        }
+      }
+      res.push(num)
+    }
+    return res
   }
 
 
